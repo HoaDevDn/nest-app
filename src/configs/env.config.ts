0c9755d61b9acd20008ccfea9d13fa0262af9d86
phu-hoa <phuhoadn95@gmail.com>
2024-10-19 12:36:04 +0700

    f5: Fix path (ENTITIES & MIGRATIONS)

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -15,6 +15,6 @@ export const env = {
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_PORT: process.env.DB_PORT,
   DB_HOST: process.env.DB_HOST,
-  ENTITIES: [process.cwd() + '**/*.entity{.ts,.js}'],
-  MIGRATIONS: [process.cwd() + 'migrations/*{.ts,.js}'],
+  ENTITIES: [process.cwd() + 'src/**/*.entity{.ts,.js}'],
+  MIGRATIONS: [process.cwd() + 'src/migrations/*{.ts,.js}'],
 };
