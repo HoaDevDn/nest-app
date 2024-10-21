808966de3c9d16bfb794efcb6e4c18989a9bd824
phu-hoa <phuhoadn95@gmail.com>
2024-10-21 22:30:10 +0700

    f6: Update path

--- src/configs/orm.config.ts
+++ src/configs/orm.config.ts
@@ -2,18 +2,21 @@ import { registerAs } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { env } from './env.config';
 
-const config = {
+const config: DataSourceOptions = {
   type: 'postgres',
   host: env.DB_HOST, // Hoặc giá trị host thực tế
   port: Number(env.DB_PORT),
   username: env.DB_USERNAME, // Thay bằng username PostgreSQL của bạn
   password: env.DB_PASSWORD, // Thay bằng password của bạn
   database: env.DB_NAME, // Thay bằng tên database bạn sử dụng
-  entities: env.ENTITIES,
-  migrations: env.MIGRATIONS,
-  autoLoadEntities: true,
+  entities: [`${env.ROOT_PATH}/src/**/*.entity.{ts,js}`],
+  // migrations: [`${env.ROOT_PATH}/migrations/*{.ts,.js}`],
   synchronize: false,
-} as DataSourceOptions;
+};
 
-export default registerAs('typeorm', () => config);
+export default registerAs('typeorm', () => ({
+  ...config,
+  autoLoadEntities: true,
+}));
+
 export const AppDataSource = new DataSource(config); // this one for migration, Don't remove.
